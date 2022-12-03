74d2deb2c0fc9990b427d2aee8c6d7d47588dbce
Rushan <ektoeror777@gmail.com>
2022-12-03 13:39:04 +0300

    005

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Add your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")// Все варианты написания имени с регистром
+
+    Console.WriteLine ("Hello, Whore!");
+else 
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
