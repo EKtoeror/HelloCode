3a932918c156763d37d245f637ab04ca9d713a1c
Rushan <ektoeror777@gmail.com>
2022-12-15 19:06:31 +0300

    Lection 3

---
+++ Cycles/Program.cs
@@ -0,0 +1,105 @@
+﻿// Цикл со счетчиком For
+
+// for(int i =0 /*начало счетчика*/; i<count /* условие счетчика*/; i++ /*шаг*/)
+
+
+// ПРИМЕРЫ 
+
+int Method4(int count, string text) // и принимает и возвращает
+{
+    int i = 0;
+    string result = "";//пустая строка (=String.Empty)
+
+    /*while (i < count)
+    {
+        result = result + text;
+        i++;
+    }*/
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(count, "text");
+Console.WriteLine(res);
+
+// Таблица умножения 
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");//интерполяция строк
+    }
+    Console.WriteLine();//пустая строка
+}
+
+//Дан текст,заменить: все пробелы - черточками, "к" на "К", "С" на "с"
+
+string text = "text";
+
+//string s ="text"
+//           0123
+// s[3] // t
+
+string Replace(string text, char oldValue, char newValue)//char - конкретный символ
+{
+    string result = String.Empty;
+
+    int size = text.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, " ", " --");
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(text, "к", " К");
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(text, "С", " с");
+Console.WriteLine(newText);
+
+//Алгоритм сортировки методом выбора
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 1, 1, };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+
+        int temp = array[i]; //обмен местами двух элементов массива
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+
---
+++ Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿// Вид 1
+
+void Method1();//ничего не принимает на вход и не возвращает на выходе
+{
+    Console.WriteLine("Hello, World!");
+}
+Method1();//вызов метода
+
+// Вид 2
+
+void Method2(string msg);//ничего не возвращает на выходе
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg: "Text");//вызов метода
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;//инкримент
+    }
+}
+Method21("Text", count);
+//Method21(msg:"Text",count:N); именованные 
+
+// выход из зациклинивания программы Ctrl+C
+
+
+// Вид 3
+
+int Method3() //не принимает но возвращает
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);//вывод метода
+
+// Вид 4 
+
+int Method4(int count, string text) // и принимает и возвращает
+{
+    int i = 0;
+    string result = "";//пустая строка (=String.Empty)
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res= Method4(count , "text");
+Console.WriteLine(res);
